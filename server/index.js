df4d7c19280a8e21285a8e11064e0602ca3648df
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-03-12 23:22:09 +0500

    Setup connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
-const express = require("express")
+import connectToDB from "./config/db.js"
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send("Hello world")
 })
 
-app.listen('3000', () => {
-    console.log("Server running on PORT 3000")
-})
+connectToDB()
+	.then(() => {
+		app.listen(process.env.PORT, () => {
+		    console.log("Connected to Database and Server running on PORT " + process.env.PORT)
+		})
+	})
+	.catch((error) => {
+		console.log(error)
+	})
