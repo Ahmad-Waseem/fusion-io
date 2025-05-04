3d27ec10ef570bba20317709f5908373aed75fcb
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-05-04 04:30:38 -0700

    remove unecessary route

--- server/index.js
+++ server/index.js
@@ -13,9 +13,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
 
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
